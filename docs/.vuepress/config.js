a958bc89fb0a14edc9b88f32c3b6a72c4c3e6395
verwo <wouter_verstraete@hotmail.com>
2025-03-10 14:18:32 +0100

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
   title: 'Linux Essentials',
   description: 'Linux Essentials Course for VIVES University of Applied Sciences (Bachelor Degree)',
 
-  theme: defaultTheme({
+  theme: hopeTheme({
     logo: '/images/linux.png',
     navbar: [
       { text: 'Toledo', link: 'https://toledo.kuleuven.be/portal' },
@@ -112,6 +112,11 @@ module.exports = {
     docsDir: 'docs',
     docsBranch: 'master'
   }),
+  plugins: {
+    mdEnhance: {
+      presentation: true,
+    },
+  },
   serviceWorker: true,
   plugins: [
     containerPlugin({
